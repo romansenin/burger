4e51eba33cd5d083ee5c846c3cd6b20f457cb7d4
rasenin <romansenin15@gmail.com>
2019-06-21 17:01:46 -0700

    added a name to the table

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ drop database if exists burgers_db;
 create database burgers_db;
 use burgers_db;
 
-create table(
+create table burgers (
   id int auto_increment,
   burger_name varchar(100) not null,
   devoured boolean default 0,

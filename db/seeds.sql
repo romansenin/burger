79978a144727f2f2c30a2c40d2dd33ddb3abc8cf
rasenin <romansenin15@gmail.com>
2019-06-21 17:09:15 -0700

    wrote seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+insert into burgers (burger_name) values ("Chicken Bacon Burger");
+insert into burgers (burger_name) values ("Beef Double Cheese Burger");
+insert into burgers (burger_name) values ("Happy Meal Burger");

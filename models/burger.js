1c5a44658902d8b21491d80f581ba63a8cfcdb85
rasenin <romansenin15@gmail.com>
2019-06-23 13:25:42 -0700

    wrote burger model

---
+++ models/burger.js
@@ -0,0 +1,22 @@
+let orm = require("../config/orm.js");
+
+let burger = {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (burgers_controller.js).
+module.exports = burger;

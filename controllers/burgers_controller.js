57dbd2b8a54405f30077e29835eef57d9be26215
rasenin <romansenin15@gmail.com>
2019-06-27 21:21:40 -0700

    added put request to eat button

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,8 +7,20 @@ let burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
   burger.selectAll(function(burgers) {
-    res.render("index", {burgers});
+    res.render("index", { burgers });
   });
+});
+
+router.put("/eat", function(req, res) {
+  console.log(req.body);
+  // burger.updateOne({ devoured: true }, "id = " + req.body.id, function(result) {
+  //   if (result.changedRows == 0) {
+  //     // If no rows were changed, then the ID must not exist, so 404
+  //     return res.status(404).end();
+  //   } else {
+  //     res.status(200).end();
+  //   }
+  // });
 });
 
 module.exports = router;

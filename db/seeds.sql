23869e07237e4b3d70996c9a2da22c0ee02df402
rasenin <romansenin15@gmail.com>
2019-06-28 01:15:10 -0700

    changed seed file to insert default state

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-insert into burgers (burger_name, devoured) values ("Cheese Burger", TRUE);
+insert into burgers (burger_name, devoured) values ("Cheese Burger");
 insert into burgers (burger_name) values ("Hamburger");
 insert into burgers (burger_name) values ("Pizza Burger");
-insert into burgers (burger_name, devoured) values ("The King Burger", TRUE);
+insert into burgers (burger_name, devoured) values ("The King Burger");

30f9a5f14a4dd42506954e181dd201f7aafc7e5c
rasenin <romansenin15@gmail.com>
2019-06-27 21:22:00 -0700

    added body parser to server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 // Sets up the Express app to handle data parsing
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 let routes = require("./controllers/burgers_controller.js");
 app.use(routes);
